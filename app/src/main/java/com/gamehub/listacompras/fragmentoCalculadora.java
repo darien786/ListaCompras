6c07285ca061297db13b2d8063b80ba6e394b6fa
SantiagoECHM <111538733+SantiagoECHM@users.noreply.github.com>
2023-05-28 16:32:06 -0700

    Calculadora Funcional

--- app/src/main/java/com/gamehub/listacompras/fragmentoCalculadora.java
+++ app/src/main/java/com/gamehub/listacompras/fragmentoCalculadora.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,7 +57,11 @@ public class fragmentoCalculadora extends Fragment {
         }
 
     }
-
+    private Button btnLista, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn, btnIgual, btnSuma, btnResta, btnMulti, btnPunto, btnBorrar, btnDividir;
+    public TextView editTextResul;
+    protected double operando1 = Double.NaN;
+    protected double operando2;
+    protected  char  operador;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -65,7 +70,194 @@ public class fragmentoCalculadora extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fragmento_calculadora, container, false);
 
+        //Creacion de los componentes botones
+        btn = view.findViewById(R.id.ceroBoton);
+        btn1 = view.findViewById(R.id.unoBoton);
+        btn2 = view.findViewById(R.id.dosBoton);
+        btn3 = view.findViewById(R.id.tresBoton);
+        btn4 = view.findViewById(R.id.cuatroBoton);
+        btn5 = view.findViewById(R.id.cincoBoton);
+        btn6 = view.findViewById(R.id.seisBoton);
+        btn7 = view.findViewById(R.id.sieteBoton);
+        btn8 = view.findViewById(R.id.ochoBoton);
+        btn9 = view.findViewById(R.id.nueveBoton);
+
+        btnIgual = view.findViewById(R.id.igualBoton);
+        btnResta = view.findViewById(R.id.restaBoton);
+        btnMulti = view.findViewById(R.id.multiplicaciónBoton);
+        btnSuma = view.findViewById(R.id.sumaBoton);
+        btnPunto = view.findViewById(R.id.puntoBoton);
+        btnBorrar = view.findViewById(R.id.borrarBoton);
+        btnDividir = view.findViewById(R.id.dividirBoton);
+
+        btnLista = view.findViewById(R.id.totalLista);
+
+        editTextResul = view.findViewById(R.id.resultadoTextView);
+
+        btnLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append("9");
+            }
+        });
+
+        btnPunto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextResul.append(".");
+            }
+        });
+
+        btnSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compute();
+                operador = '+';
+                editTextResul.setText(null);
+            }
+        });
+
+        btnResta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compute();
+                operador = '-';
+                editTextResul.setText(null);
+            }
+        });
+
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compute();
+                operador = '*';
+                editTextResul.setText(null);
+            }
+        });
+
+        btnDividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compute();
+                operador = '/';
+                editTextResul.setText(null);
+            }
+        });
+
+        // Establecer listener para el botón Equals
+        btnIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compute();
+                operador = '=';
+                editTextResul.setText(String.valueOf(operando1));
+                operando1 = Double.NaN;
+            }
+        });
+
+        // Establecer listener para el botón Clear
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operando1 = Double.NaN;
+                operando2 = Double.NaN;
+                editTextResul.setText(null);
+            }
+        });
+
         return view;
     }
 
+    private void compute() {
+        if (!Double.isNaN(operando1)) {
+            operando2 = Double.parseDouble(editTextResul.getText().toString());
+            switch (operador) {
+                case '+':
+                    operando1 += operando2;
+                    break;
+                case '-':
+                    operando1 -= operando2;
+                    break;
+                case '*':
+                    operando1 *= operando2;
+                    break;
+                case '/':
+                    operando1 /= operando2;
+                    break;
+                case '=':
+                    break;
+            }
+        } else {
+            operando1 = Double.parseDouble(editTextResul.getText().toString());
+        }
+    }
 }
